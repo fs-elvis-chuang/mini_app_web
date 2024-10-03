a3dea5d90a537925fddf516f8f262c98ae3ed510
fs-elvis-chuang <Elvis.Chuang@gmail.com>
2024-10-03 12:32:07 +0800

    第一版網頁版並發布他

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
+/*
+    註解
+ */
+// 取得 h1元素
 var myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "Hello world!~~~~~";
+
+// 取得 h1元素
+var myHTML = document.querySelector("html");
+myHTML.onclick = function () {
+    alert("Ouch! Stop poking me!!!!!!!!!!!!!!!!!!");
+};
+
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
